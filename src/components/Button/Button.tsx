e3028a163832008dc0bed5675e8a6a0202c7e6a3
Nahyan Sharvin <nahyan.sharvin@katzion.com>
2023-12-14 10:11:48 +0530

    feat: add button with styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import "./Button.css"
+import React from "react";
+import "./Button.css";
+import { useDispatch } from "react-redux";
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
+export interface ButtonProps {
+  primary?: boolean;
+  size?: "small" | "medium" | "large";
 }
 
-export default Button
+const Button = ({
+  primary = false,
+  size = "medium",
+  ...props
+}: ButtonProps) => {
+  const dispatch = useDispatch();
+  const mode = primary ? "btn--primary" : "btn--secondary";
+  const action = primary ? {type: "counter/increment"} : {type: "counter/decrement"};
+
+  return (
+    <button className={`btn btn--${size} ${mode}`} onClick={() => dispatch(action)} {...props}>
+      {primary ? "Increment" : "Decrement"}
+    </button>
+  );
+};
+
+export default Button;
